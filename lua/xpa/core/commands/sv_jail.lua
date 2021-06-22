1095daa4a98d84de5ecac751fc13f644c11e30ca
crester <druggygangsta@gmail.com>
2021-06-22 20:17:40 +0700

    jail corrections

--- lua/xpa/core/commands/sv_jail.lua
+++ lua/xpa/core/commands/sv_jail.lua
@@ -1,5 +1,17 @@
-hook.Add("CanPlayerSuicide", "XPA Jail", function(pl)
-	if XPA.Jail[pl:SteamID()] then
-		return false
+local tag = "XPA Jail"
+local function isJailed(pl)
+	if XPA.Jail[pl:SteamID()] then 
+		return false 
 	end
-end)
+end
+
+hook.Add("CanPlayerSuicide", tag, isJailed)
+hook.Add("PlayerNoClip", tag, isJailed)
+hook.Add("PlayerLoadout", tag, isJailed)
+hook.Add("PlayerSpawnObject", tag, isJailed)
+hook.Add("PlayerSpawnNPC", tag, isJailed)
+hook.Add("PlayerSpawnSENT", tag, isJailed)
+hook.Add("PlayerSpawnVehicle", tag, isJailed)
+hook.Add("PlayerSpawnSWEP", tag, isJailed)
+hook.Add("PlayerGiveSWEP", tag, isJailed)
+hook.Add("CanPlayerSuicide", tag, isJailed)
